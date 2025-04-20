4c46188a5db27c7d465000a1cd63435d10d5a497
Arghavan Monajemi <monajemi.am@gmail.com>
2025-04-21 00:27:34 +0330

    Configuration od the DB Connection.

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=True)
+# noinspection PyTypeChecker
+AsyncSessionLocal = sessionmaker(
+    bind= engine,
+    class_= AsyncSession,
+    expire_on_commit=False
+)
+Base = declarative_base()
